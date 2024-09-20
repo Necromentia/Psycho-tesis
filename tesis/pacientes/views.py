b51bb66778b806713ac1c8c86186c1f3add6bd42
Manuel <manuel.gonzalez@haulmerinc.onmicrosoft.com>
2024-09-20 16:32:48 -0300

    pdf listo

--- tesis/pacientes/views.py
+++ tesis/pacientes/views.py
@@ -472,8 +472,9 @@ def delete_folder(request):
 def download_patient_pdf(request, patient_id):
     try:
         patient = Patient.objects.get(id=patient_id)
-        
-
+        diagnosis = Diagnosis.objects.filter(patient=patient).first()
+        medical_history = MedicalHistory.objects.filter(patient=patient).first()
+        symptoms = Symptom.objects.filter(patient=patient).first()
         # Crear el objeto HttpResponse con el tipo de contenido PDF
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = f'attachment; filename="Paciente_{patient.first_name}_{patient.last_name}.pdf"'
@@ -485,20 +486,22 @@ def download_patient_pdf(request, patient_id):
         # Aquí agregaremos el contenido del PDF
 
         # Título Principal
-        elements.append(Paragraph("Informe del Paciente", styles['Title']))
+        elements.append(Paragraph("Informe de paciente", styles['Title']))
         elements.append(Spacer(1, 12))
         
         # Nombre del Paciente
         elements.append(Paragraph(f"{patient.first_name} {patient.last_name}", styles['Heading2']))
         elements.append(Spacer(1, 12))
+        elements.append(Paragraph(f"RUT: {patient.rut or 'No especificado'}", styles['Heading2']))
+        elements.append(Spacer(1, 12))
         # Información Personal
-        elements.append(Paragraph("Información Personal", styles['Heading3']))
+        elements.append(Paragraph("Información personal", styles['Heading3']))
         elements.append(Spacer(1, 12))
 
         personal_info = [
             f"<strong>Nombre:</strong> {patient.first_name} {patient.last_name}",
             f"<strong>Género:</strong> {patient.genre}",
-            f"<strong>Fecha de Nacimiento:</strong> {patient.birth_date.strftime('%d/%m/%Y')}",
+            f"<strong>Fecha de nacimiento:</strong> {patient.birth_date.strftime('%d/%m/%Y')}",
             f"<strong>Edad:</strong> {patient.age} años",
             f"<strong>Dirección:</strong> {patient.address or 'No especificada'}",
             f"<strong>Teléfono:</strong> {patient.phone or 'No especificado'}",
@@ -514,30 +517,30 @@ def download_patient_pdf(request, patient_id):
             elements.append(Spacer(1, 6))
         # Historial Médico
         elements.append(Spacer(1, 12))
-        elements.append(Paragraph("Historial Médico", styles['Heading3']))
+        elements.append(Paragraph("Historial personal", styles['Heading3']))
         elements.append(Spacer(1, 12))
-        elements.append(Paragraph(f"{patient.personal_history or 'No hay historial personal'}", styles['Normal']))
+        elements.append(Paragraph(f"{medical_history.personal_history or 'No hay historial personal'}", styles['Normal']))
         # Historial Familiar
         elements.append(Spacer(1, 12))
-        elements.append(Paragraph("Historial Familiar", styles['Heading3']))
+        elements.append(Paragraph("Historial familiar", styles['Heading3']))
         elements.append(Spacer(1, 12))
-        elements.append(Paragraph(f"{patient.family_history or 'No hay historial familiar'}", styles['Normal']))
+        elements.append(Paragraph(f"{medical_history.family_history or 'No hay historial familiar'}", styles['Normal']))
 
         # Historial Clínico
         elements.append(Spacer(1, 12))
-        elements.append(Paragraph("Historial Clínico", styles['Heading3']))
+        elements.append(Paragraph("Historial clínico", styles['Heading3']))
         elements.append(Spacer(1, 12))
-        elements.append(Paragraph(f"{patient.clinical_history or 'No hay historial clínico'}", styles['Normal']))
+        elements.append(Paragraph(f"{medical_history.clinical_history or 'No hay historial clínico'}", styles['Normal']))
         # Síntomas
         elements.append(Spacer(1, 12))
         elements.append(Paragraph("Síntomas", styles['Heading3']))
         elements.append(Spacer(1, 12))
 
         symptoms = [
-            f"<strong>Síntomas Físicos:</strong> {patient.physical_symptoms or 'No hay síntomas físicos'}",
-            f"<strong>Funcionamiento Social:</strong> {patient.social_symptoms or 'No hay síntomas sociales'}",
-            f"<strong>Síntomas Emocionales:</strong> {patient.emotional_symptoms or 'No hay síntomas emocionales'}",
-            f"<strong>Síntomas de Comportamiento:</strong> {patient.behavioral_symptoms or 'No hay síntomas de comportamiento'}",
+            f"<strong>Síntomas físicos:</strong> {symptoms.physical_symptoms or 'No hay síntomas físicos'}",
+            f"<strong>Funcionamiento social:</strong> {symptoms.social_symptoms or 'No hay síntomas sociales'}",
+            f"<strong>Síntomas emocionales:</strong> {symptoms.emotional_symptoms or 'No hay síntomas emocionales'}",
+            f"<strong>Síntomas de comportamiento:</strong> {symptoms.behavioral_symptoms or 'No hay síntomas de comportamiento'}",
         ]
 
         for symptom in symptoms:
@@ -545,16 +548,15 @@ def download_patient_pdf(request, patient_id):
             elements.append(Spacer(1, 6))
         # Hipótesis Diagnóstica
         elements.append(Spacer(1, 12))
-        elements.append(Paragraph("Hipótesis Diagnóstica", styles['Heading3']))
+        elements.append(Paragraph("Hipótesis diagnóstica", styles['Heading3']))
         elements.append(Spacer(1, 12))
 
-        diagnosis = patient.diagnosis_set.last()
         if diagnosis:
             elements.append(Paragraph(f"{diagnosis.diagnosis or 'Sin diagnóstico'}", styles['Normal']))
             elements.append(Spacer(1, 6))
-            elements.append(Paragraph(f"<strong>Detalles del Diagnóstico:</strong> {diagnosis.details or 'No hay detalles'}", styles['Normal']))
+            elements.append(Paragraph(f"<strong>Detalles del diagnóstico:</strong> {diagnosis.details or 'No hay detalles'}", styles['Normal']))
             elements.append(Spacer(1, 6))
-            elements.append(Paragraph(f"<strong>Diagnóstico Adicional:</strong> {diagnosis.additional_diagnosis or 'No hay diagnósticos adicionales'}", styles['Normal']))
+            elements.append(Paragraph(f"<strong>Diagnóstico adicional:</strong> {diagnosis.additional_diagnosis or 'No hay diagnósticos adicionales'}", styles['Normal']))
         else:
             elements.append(Paragraph("Sin diagnóstico", styles['Normal']))
 
